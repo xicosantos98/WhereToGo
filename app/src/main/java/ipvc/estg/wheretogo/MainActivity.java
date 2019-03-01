ff3381b819539853d6cf9a5d42e761ba12fdb123
gabri <8dgabrielsantos@gmail.com>
2019-03-01 16:42:18 +0000

    sada

--- app/src/main/java/ipvc/estg/wheretogo/MainActivity.java
+++ app/src/main/java/ipvc/estg/wheretogo/MainActivity.java
@@ -2,11 +2,13 @@ package ipvc.estg.wheretogo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("123","123");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
